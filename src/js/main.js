3095f87d8eec5261fe323fc11e19b64fad49bf18
rakhagans37 <muhammadrakha3704@gmail.com>
2024-03-05 19:42:43 +0700

    Update : fixing lighthouse score

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,27 @@
-import CountUp from "countup.js";
-import Odometer from "odometer_countup.js";
+import { CountUp } from "countup.js";
+import { Odometer } from "odometer.js";
 
 window.onload = function () {
-    var countUpEv = new CountUp("event", 3, {
-        suffix: "+",
-        plugin: new Odometer({ duration: 1, lastDigitDelay: 0.5 }),
-    });
-    var countUpEx = new CountUp("experience", 2, {
-        suffix: "+",
-        plugin: new Odometer({ duration: 1, lastDigitDelay: 0.5 }),
-    });
-    var countUpPro = new CountUp("project", 3, {
-        suffix: "+",
-        plugin: new Odometer({ duration: 1, lastDigitDelay: 0.5 }),
-    });
+    try {
+        var countUpEv = new CountUp("event", 3, {
+            suffix: "+",
+            plugin: new Odometer({ duration: 1, lastDigitDelay: 0.5 }),
+        });
+        var countUpEx = new CountUp("experience", 2, {
+            suffix: "+",
+            plugin: new Odometer({ duration: 1, lastDigitDelay: 0.5 }),
+        });
+        var countUpPro = new CountUp("project", 3, {
+            suffix: "+",
+            plugin: new Odometer({ duration: 1, lastDigitDelay: 0.5 }),
+        });
 
-    countUpEv.start();
-    countUpEx.start();
-    countUpPro.start();
+        countUpEv.start();
+        countUpEx.start();
+        countUpPro.start();
+    } catch (error) {
+        document.getElementById("event").innerHTML = "3+";
+        document.getElementById("experience").innerHTML = "2+";
+        document.getElementById("project").innerHTML = "3+";
+    }
 };
