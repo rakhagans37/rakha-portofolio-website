adc27a2d48b4dc72f293104017222d84094c44d9
rakhagans37 <muhammadrakha3704@gmail.com>
2024-03-06 13:57:07 +0700

    Update : fixing problems while importing countupjs module

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,19 @@
-import { CountUp } from "../../node_modules/countup.js/dist/countUp.min.js";
-import { Odometer } from "../../node_modules/odometer_countup/dist/odometer.min.js";
-
 window.onload = function () {
     try {
-        var countUpEv = new CountUp("event", 3, {
+        var countUpEv = new countUp.CountUp("event", 3, {
             suffix: "+",
-            plugin: new Odometer({ duration: 1, lastDigitDelay: 0.5 }),
         });
-        var countUpEx = new CountUp("experience", 2, {
+        var countUpEx = new countUp.CountUp("experience", 2, {
             suffix: "+",
-            plugin: new Odometer({ duration: 1, lastDigitDelay: 0.5 }),
         });
-        var countUpPro = new CountUp("project", 3, {
+        var countUpPro = new countUp.CountUp("project", 3, {
             suffix: "+",
-            plugin: new Odometer({ duration: 1, lastDigitDelay: 0.5 }),
         });
 
         countUpEv.start();
         countUpEx.start();
         countUpPro.start();
     } catch (error) {
-        document.getElementById("event").innerHTML = "3+";
-        document.getElementById("experience").innerHTML = "2+";
-        document.getElementById("project").innerHTML = "3+";
+        console.log(error);
     }
 };
